643a17465d62e13d9ccdb1f1e39f058af6f3320b
pbes0707 <pbes0707@gmail.com>
2015-01-24 21:22:29 +0900

    Library UI prototype clear

---
+++ Wordpedia-window_phone/Wordpedia-window_phone/Library.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Wordpedia_window_phone
+{
+    public sealed partial class Library : Page
+    {
+        public Library()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            tb_search.Width = Window.Current.Bounds.Width - 130;
+            lv_CollectionList.Height = Window.Current.Bounds.Height - 225;
+
+            initialize();
+        }
+
+        private void initialize()
+        {
+            List<collectionData> list = new List<collectionData>();
+
+            //////////////////////All Vocabulary Load/////////////////////////
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(new collectionData()
+                {
+                    Title = "Title : " + i.ToString(),
+                    Date = i.ToString(),
+                    Translate = "translate : " + i.ToString()
+                });
+            }
+            /////////////////////////////////////////////////////////////////
+
+            lv_CollectionList.ItemsSource = list;
+        }
+    }
+}
---
+++ Wordpedia-window_phone/Wordpedia-window_phone/collectionData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordpedia_window_phone
+{
+    class collectionData
+    {
+        public String Title { get; set; }
+        public String Date { get; set; }
+        public String Translate { get; set; }
+    }
+}
