61ae867e570ae7270b1a267f6ee9fbdb91911dda
pbes0707 <pbes0707@naver.com>
2015-03-12 06:14:33 +0900

    lots of programming

--- Wordpedia-window_phone/Wordpedia_windows/Wordpedia_windows.Windows/MainPage.xaml.cs
+++ Wordpedia-window_phone/Wordpedia_windows/Wordpedia_windows.Windows/MainPage.xaml.cs
@@ -4,11 +4,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Graphics.Display;
+using Windows.Storage.FileProperties;
+using Windows.Storage.Streams;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -17,7 +23,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using WindowsPreview.Media.Ocr;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -29,8 +37,14 @@ namespace Wordpedia_windows
     public sealed partial class MainPage : Page
     {
         private List<navigation_item> navigation_list;
+        private List<vocaData> voca_list;
         private navigation_item prev_navItem;
+        private String[] words;
+        private WriteableBitmap bitmap;
         private bool isLogin = false;
+        private userData _userData;
+
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -43,26 +57,39 @@ namespace Wordpedia_windows
             /////////////////////Navigation Initialize////////////////////
             navigation_list = new List<navigation_item>();
             prev_navItem = new navigation_item();
+            voca_list = new List<vocaData>();
             prev_navItem.Id = 0;
 
             navigation_item v = new navigation_item();
             v.Id = 1;
-            v.Text = "User Email";
+            v.Text = "User Id";
+            v.ImgSrc = "Assets/Navigation/user_icon.png";
             navigation_list.Add(v.Copy());
 
             v.Id = 2;
             v.Text = "Library";
+            v.ImgSrc = "Assets/Navigation/library_icon.png";
             navigation_list.Add(v.Copy());
 
             v.Id = 3;
             v.Text = "Stats";
+            v.ImgSrc = "Assets/Navigation/stats_icon.png";
             navigation_list.Add(v.Copy());
 
             v.Id = 4;
             v.Text = "Surf";
+            v.ImgSrc = "Assets/Navigation/surf_icon.png";
             navigation_list.Add(v.Copy());
 
             lv_navigation.ItemsSource = navigation_list;
+
+            String[] _language = {"English","Czech","Danish","German","Greek","English","Spanish",
+                                "Finnish","French","Hungarian","Italian","Japanese","Korean","Dutch",
+                                "Norwegian","Polish","Russian","Swedish","Turkish","ChineseSimplified","Portuguese"};
+
+            cbx_createVoca_ocrlanguage.ItemsSource = _language;
+            cbx_createVoca_ocrlanguage.SelectedItem = "English";
+
 
             /////////////////Design Initialize///////////////////////////
             Grid_Navigation.Width = 300;
@@ -70,6 +97,9 @@ namespace Wordpedia_windows
 
             Grid_UserEmail.Width = Window.Current.Bounds.Width - 300;
             Grid_UserEmail.Height = Window.Current.Bounds.Height;
+
+            cbx_createVoca_language.ItemsSource = LanguageCode.getLanguageName();
+            cbx_createVoca_language.SelectedItem = "Korean";
 
             //////////////////App Start Login//////////////////////////////
 
@@ -103,7 +133,7 @@ namespace Wordpedia_windows
                         }
                         else // 로그인이 되있지 않을 경우
                         {
-                            ChangeGridVisibility(Grid_UserEmail_regist, Visibility.Visible);
+                            ChangeGridVisibility(Grid_UserEmail_LoginRegist, Visibility.Visible);
                         }
                         break;
                     case 2:
@@ -130,10 +160,11 @@ namespace Wordpedia_windows
                 Grid_UserEmail.Visibility = Visibility.Collapsed;
                 Grid_Library.Visibility = Visibility.Collapsed;
 
-                Grid_UserEmail_regist.Visibility = Visibility.Collapsed;
-                Grid_UserEmail_login.Visibility = Visibility.Collapsed;
+                Grid_UserEmail_LoginRegist.Visibility = Visibility.Collapsed;
                 Grid_UserEmail_isLogin.Visibility = Visibility.Collapsed;
                 Grid_Library_list.Visibility = Visibility.Collapsed;
+                Grid_Library_voca.Visibility = Visibility.Collapsed;
+                Grid_Library_createVoca.Visibility = Visibility.Collapsed;
                 //자신 이외의 다른 그리드를 모두 비활성화
             }
             grid.Visibility = visible;
@@ -142,40 +173,27 @@ namespace Wordpedia_windows
         {
             public int Id { get; set; }
             public String Text { get; set; }
+            public String ImgSrc { get; set; }
 
             public navigation_item Copy()
             {
                 navigation_item v = new navigation_item();
                 v.Id = Id;
                 v.Text = Text;
+                v.ImgSrc = ImgSrc;
                 return v;
             }
         }
 
-
         ///////////////Regist//////////////////
-        private void btn_regist_signin_Click(object sender, RoutedEventArgs e)
+        private void btn_loginregist_signin_Click(object sender, RoutedEventArgs e)
         {
-            registFunction(tbx_regist_ID.Text, tbx_regist_passwd.Password);
+            registFunction(tbx_loginregist_ID.Text, tbx_loginregist_passwd.Password);
         }
-        private void btn_regist_login_Click(object sender, RoutedEventArgs e)
+        private void btn_loginregist_confirm_Click(object sender, RoutedEventArgs e)
         {
-            ChangeGridVisibility(Grid_UserEmail_regist, Visibility.Collapsed);
-            ChangeGridVisibility(Grid_UserEmail_login, Visibility.Visible);
+            loginFunction(tbx_loginregist_ID.Text, tbx_loginregist_passwd.Password);
         }
-
-        ///////////////Login///////////////////
-        private void btn_login_confirm_Click(object sender, RoutedEventArgs e)
-        {
-            loginFunction(tbx_login_ID.Text, tbx_login_passwd.Password);
-        }
-        private void btn_login_regist_Click(object sender, RoutedEventArgs e)
-        {
-            ChangeGridVisibility(Grid_UserEmail_login, Visibility.Collapsed);
-            ChangeGridVisibility(Grid_UserEmail_regist, Visibility.Visible);
-        }
-
-        //////////// isLogin//////////////////////
         private void btn_isLogin_signout_Click(object sender, RoutedEventArgs e)
         {
 
@@ -187,11 +205,18 @@ namespace Wordpedia_windows
             localSettings.Values.Remove("token");
 
             ChangeGridVisibility(Grid_UserEmail_isLogin, Visibility.Collapsed);
-            ChangeGridVisibility(Grid_UserEmail_login, Visibility.Visible);
+            ChangeGridVisibility(Grid_UserEmail_LoginRegist, Visibility.Visible);
+
+
+            foreach (navigation_item v in navigation_list)
+                if (v.Id == 1)
+                    v.Text = "User Id";
+
+            lv_navigation.ItemsSource = null;
+            lv_navigation.ItemsSource = navigation_list;
 
             isLogin = false;
         }
-
 
         private async void loginFunction(String _ID, String _pw, bool show = true)
         {
@@ -224,11 +249,11 @@ namespace Wordpedia_windows
 
                         tb_isLogin_welcome.Text = "Welcome to " + _id;
 
-                        ChangeGridVisibility(Grid_UserEmail_login, Visibility.Collapsed);
-                        if(show==true) ChangeGridVisibility(Grid_UserEmail_isLogin, Visibility.Visible);
+                        ChangeGridVisibility(Grid_UserEmail_LoginRegist, Visibility.Collapsed);
+                        if (show == true) ChangeGridVisibility(Grid_UserEmail_isLogin, Visibility.Visible);
 
-                        tbx_login_ID.Text = "";
-                        tbx_login_passwd.Password = "";
+                        tbx_loginregist_ID.Text = "";
+                        tbx_loginregist_passwd.Password = "";
 
                         isLogin = true;
                     }
@@ -238,7 +263,15 @@ namespace Wordpedia_windows
 
                     }
                     MessageDialog msg = new MessageDialog(jsonArray["requestMessage"].ToString(), state);
-                    if(show==true) await msg.ShowAsync();
+                    if (show == true) await msg.ShowAsync();
+
+                    foreach (navigation_item v in navigation_list)
+                        if (v.Id == 1)
+                            v.Text = _ID;
+
+                    lv_navigation.ItemsSource = null;
+                    lv_navigation.ItemsSource = navigation_list;
+
                 }
             }
             catch (Exception)
@@ -264,8 +297,8 @@ namespace Wordpedia_windows
                     {
                         state = "SUCCESS";
 
-                        tbx_regist_ID.Text = "";
-                        tbx_regist_passwd.Password = "";
+                        tbx_loginregist_ID.Text = "";
+                        tbx_loginregist_passwd.Password = "";
                     }
                     else
                     {
@@ -279,6 +312,9 @@ namespace Wordpedia_windows
             {
             }
         }
+
+
+        ///////////////Library////////////////////
         private async void loadLibraryList()
         {
             string url = "http://wordpedia.herokuapp.com/get/collection/user";
@@ -294,11 +330,13 @@ namespace Wordpedia_windows
                 {
                     StreamReader reader = new StreamReader(stream, Encoding.UTF8);
                     String responseString = reader.ReadToEnd();
-                    userData _userData =
+                    _userData =
                         JsonConvert.DeserializeObject<userData>(responseString);
+                    lv_voca.ItemsSource = _userData.collections;
+                    voca_list = _userData.collections;
                 }
             }
-            catch(Exception)
+            catch (Exception)
             {
 
             }
@@ -323,8 +361,196 @@ namespace Wordpedia_windows
             }
             catch (Exception)
             {
-
             }
         }
+
+        private void tbx_loginregist_ID_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+        private void sb_search_SuggestionsRequested(SearchBox sender, SearchBoxSuggestionsRequestedEventArgs args)
+        {
+        }
+        private void sb_search_QuerySubmitted(SearchBox sender, SearchBoxQuerySubmittedEventArgs args)
+        {
+            List<vocaData> temp_voca = new List<vocaData>();
+            string query = args.QueryText;
+            foreach (vocaData v in voca_list)
+                if (v.createDate.ToLower().Contains(query.ToLower()) || v.creator.ToLower().Contains(query.ToLower())
+                    || v.title.ToLower().Contains(query.ToLower()) || v.to.ToLower().Contains(query.ToLower()))
+                    temp_voca.Add(v);
+            lv_voca.ItemsSource = temp_voca;
+        }
+        private void lv_voca_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lv_voca.SelectedItems.Count == 0) return;
+            vocaData v = lv_voca.SelectedItems[0] as vocaData;
+
+            ChangeGridVisibility(Grid_Library_list, Visibility.Collapsed);
+            ChangeGridVisibility(Grid_Library_voca, Visibility.Visible);
+
+            tb_Library_title.Text = v.title;
+            tb_Library_createDate.Text = v.createDate;
+            tb_Library_translate.Text = v.fullTranslate;
+
+            List<Word> temp_list = new List<Word>();
+            foreach (Word w in v.wordList)
+                if (!w.translateWord.Equals(w.word))
+                    temp_list.Add(w);
+
+            lv_Library_word.ItemsSource = temp_list;
+        }
+        private async void btn_capture_Click(object sender, RoutedEventArgs e)
+        {
+            DataPackageView dataPackageView = Clipboard.GetContent();
+            if (dataPackageView.Contains(StandardDataFormats.Bitmap))
+            {
+                ////////////원문 언어가 무엇인지 팝업을 띄워주어야 한다////////////
+                int[] _code = { 1028, 1029, 1030, 1031, 1032, 1033, 1034, 1035, 1036, 1038, 1040, 1041, 1042, 1043, 1044, 1045, 1049, 1053, 1055, 2052, 2070 };
+                IRandomAccessStreamReference imageReceived = null;
+                try
+                {
+                    imageReceived = await dataPackageView.GetBitmapAsync();
+                }
+                catch (Exception ex)
+                {
+                }
+
+                if (imageReceived != null)
+                {
+                    using (IRandomAccessStream imageStream = await imageReceived.OpenReadAsync())
+                    {
+                        BitmapImage bitmapImage = new BitmapImage();
+                        bitmapImage.SetSource(imageStream);
+
+                        IRandomAccessStream writeBitmapStream = await imageReceived.OpenReadAsync();
+                        bitmap = new WriteableBitmap(
+                            bitmapImage.PixelWidth, bitmapImage.PixelHeight);
+                        bitmap.SetSource(writeBitmapStream);
+
+                        ChangeGridVisibility(Grid_Library_list, Visibility.Collapsed);
+                        ChangeGridVisibility(Grid_Library_createVoca, Visibility.Visible);
+
+                    }
+                }
+            }
+            else
+            {
+                var dialog = new MessageDialog("Your Clipboard data is not Image", "Error");
+                await dialog.ShowAsync();
+            }
+            //this.sex.Source = await CreateBitmapFromElement(this.Parent as FrameworkElement);
+        }
+
+
+        /////////////////////Create Vocabulary//////////////////
+        private async void btn_create_vocabulary_Click(object sender, RoutedEventArgs e)
+        {
+
+            OcrLanguage olc = (OcrLanguage)Enum.Parse(typeof(OcrLanguage), cbx_createVoca_ocrlanguage.SelectedItem as String);
+            OcrEngine ocrEngine = new OcrEngine(olc);
+            var ocrResult = await ocrEngine.RecognizeAsync(
+                (uint)bitmap.PixelHeight,
+                (uint)bitmap.PixelWidth,
+                bitmap.PixelBuffer.ToArray());
+
+            StringBuilder ocr = new StringBuilder();
+            if (ocrResult.Lines != null)
+            {
+                foreach (var line in ocrResult.Lines)
+                {
+                    string newLine = string.Empty;
+                    foreach (var word in line.Words)
+                    {
+                        newLine = newLine + word.Text + " ";
+                    }
+                    ocr.AppendLine(newLine);
+                }
+                String Article = ocr.ToString();
+                String transArticle = Article.ToLower();
+                /*Regex regex = new Regex("<[^>]*>", RegexOptions.IgnoreCase);
+                String result = regex.Replace(htmlContent, "");
+                result = result.Replace("\n", "");*/
+                transArticle = Regex.Replace(transArticle, @"[^a-zA-Z0-9가-힣]", ",", RegexOptions.IgnoreCase);
+                String[] separators = { "," };
+                words = transArticle.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                if (words[0] != null)
+                {
+                    ChangeGridVisibility(Grid_Library_list, Visibility.Collapsed);
+                    ChangeGridVisibility(Grid_Library_createVoca, Visibility.Visible);
+                }
+                else
+                {
+                    var dialog = new MessageDialog("No Vocabulary in Article", "ERROR");
+                    await dialog.ShowAsync();
+                }
+            }
+            else
+            {
+                ///////////No Vocabulary/////////////
+                var dialog = new MessageDialog("No Vocabulary in Picture", "ERROR");
+                await dialog.ShowAsync();
+            }
+            String title = tbx_createVoca_title.Text;
+            String to = LanguageCode.getLanguageCode()[cbx_createVoca_language.SelectedIndex];
+            List<String> fixed_words = new List<String>();
+            foreach(String v in words)
+            {
+                if (v.Length < 2) continue;
+                bool flag = false;
+                foreach(String w in fixed_words)
+                    if( v.Equals(w) == true)
+                        flag = true;
+                if (flag == false)
+                    fixed_words.Add(v);
+            }
+            String query = fixed_words[0];
+            for(int i = 1 ;i<fixed_words.Count ; i++)
+            {
+                query += "&w=" + fixed_words[i];
+            }
+
+            Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            string url = "http://wordpedia.herokuapp.com/collection/create?title=" + title + "&to=" + to + "&w=" + query;
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "POST";
+            request.Headers["token"] = localSettings.Values["token"].ToString();
+            try
+            {
+                WebResponse response = await request.GetResponseAsync();
+                using (Stream stream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                    String responseString = reader.ReadToEnd();
+                    Dictionary<string, string> jsonArray =
+                        JsonConvert.DeserializeObject<Dictionary<string, string>>(responseString);
+                    String state;
+                    if (jsonArray["requestCode"].Equals("1"))
+                    {
+                        state = "SUCCESS";
+                        tbx_createVoca_title.Text = "";
+
+                        loadLibraryList();
+                        ChangeGridVisibility(Grid_Library_createVoca, Visibility.Collapsed);
+                        ChangeGridVisibility(Grid_Library_list, Visibility.Visible);
+                    }
+                    else
+                    {
+                        state = "ERROR";
+                    }
+                    MessageDialog msg = new MessageDialog(jsonArray["requestMessage"], state);
+                    await msg.ShowAsync();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+        private void cbx_createVoca_language_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
     }
 }
